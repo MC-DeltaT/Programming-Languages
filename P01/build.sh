edf84d8ce501c251123e2b9719d1b3717f971661
Reece Jones <reece.jones131@gmail.com>
2020-08-06 15:46:33 +0800

    Fixed print format.

--- P01/build.sh
+++ P01/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# For some reason the `-static` linking option is required otherwise the program doesn't do anything.
-f77 -o fizzbuzz -static fizzbuzz.f
+f77 -o fizzbuzz fizzbuzz.f
--- P01/fizzbuzz.f
+++ P01/fizzbuzz.f
@@ -10,13 +10,13 @@ c     For loop from 1 to 100, inclusive
           div3 = mod(i, 3) .EQ. 0
           div5 = mod(i, 5) .EQ. 0
           if (div3 .AND. div5) then
-              write(*, *) 'Fizz buzz'
+              write(*, '(A)') 'Fizz buzz'
           elseif (div3) then
-              write(*, *) 'Fizz'
+              write(*, '(A)') 'Fizz'
           elseif (div5) then
-              write(*, *) 'Buzz'
+              write(*, '(A)') 'Buzz'
           else
-              write(*, *) i
+              write(*, '(I0)') i
           endif
 10    continue
 
