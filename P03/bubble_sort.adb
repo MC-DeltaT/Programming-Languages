f4a0d12ce4ffd1832df98c523a6d76ba3a3ea9ec
Reece Jones <reece.jones131@gmail.com>
2020-08-26 23:46:12 +0800

    P03: code improvements, report.

--- P03/bubble_sort.adb
+++ P03/bubble_sort.adb
@@ -22,12 +22,19 @@ procedure Bubble_Sort is
         end loop;
     end Do_Bubble_Sort;
 
-    A: Int_Array (1..15) := (9, 81, 92, 25, 38, 23, 4, 37, 34, 5, 45, 63, 57, 53, 11);
+    A: Int_Array (1..10) := (7, 6, 3, 5, 9, 4, 8, 10, 1, 2);
+--    A: Int_Array (1..10) := (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+--    A: Int_Array (1..10) := (10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+--    A: Int_Array (1..1) := (others => 42);
+--    A: Int_Array (1..0);
 begin
     Do_Bubble_Sort(A);
 
     for I in A'Range loop
         Ada.Integer_Text_IO.Put(A(I), 0);
-        Ada.Text_IO.New_Line;
+        if I /= A'Last then
+            Ada.Text_IO.Put(", ");
+        end if;
     end loop;
+    Ada.Text_IO.New_Line;
 end Bubble_Sort;
